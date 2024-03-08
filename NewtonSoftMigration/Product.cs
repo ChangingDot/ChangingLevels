5e9846b1c1577f5ba9b78b4f91dff2a3dcb04587
Matthieu Rossignon <matthieu.rossignon@hotmail.fr>
2024-03-08 14:44:34 +0100

    add first newtonsoft json files

---
+++ NewtonSoftMigration/Product.cs
@@ -0,0 +1,7 @@
+public class Product
+{
+    public required string Name { get; set; }
+    public DateTime Expiry { get; set; }
+    public required string[] Sizes { get; set; }
+}
+
---
+++ NewtonSoftMigration/Serializer.cs
@@ -0,0 +1,14 @@
+using Newtonsoft.Json;
+
+public class JsonSerializer
+{
+    public static string SerializeObject<T>(T objectToSerialize)
+    {
+        return JsonConvert.SerializeObject(objectToSerialize);
+    }
+
+    public static T DeserializeObject<T>(string jsonString)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonString);
+    }
+}
